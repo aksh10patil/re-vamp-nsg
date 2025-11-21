34cb4bf2f2da3e7195566e5983e598f3b210bc57
aksh10patil <rajdeeppatil2021@gmail.com>
2025-11-21 23:22:01 +0530

    fixed the ssr issues

--- components/AutomatedTasksCard.tsx
+++ components/AutomatedTasksCard.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+"use client"
+
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import {
   FaCalendarAlt,
